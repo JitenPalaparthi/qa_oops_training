53c3bf40d1226cc2c124300d2784ca9aef72c656
Jiten Palaparthi <JPalaparthi@vmware.com>
2022-06-27 15:36:43 +0530

    Add ayyaylist in java

---
+++ java/arrayList/Program.java
@@ -0,0 +1,36 @@
+//import java.util.ArrayList;
+import java.util.*;
+public class Program{
+    public static void main(String[] args){
+        ArrayList arr = new ArrayList();
+        /*arr.Add("Jiten");
+        arr.Add("Ash");
+        arr.Add("Dave");
+        arr.Add("Syed");
+        arr.Add("Kenneth");*/
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println("Enter a name->"+ i);
+            String name = scanner.nextLine();
+            arr.add(name);
+        }
+        System.out.println("\n\n--- Names are ----");
+       
+        for (int i=0;i<arr.size();i++)
+        {
+            System.out.println("Name-->"+(i+1)+": "+  arr.get(i));
+            
+        }
+        System.out.println("Enter a number to delete from the list");
+        int d = scanner.nextInt();
+        arr.remove(d-1);
+        arr.set(0,"Jitendranadh");
+        System.out.println("\n\n--- Names after deletion are ----");
+        for (int i=0;i<arr.size();i++)
+        {
+            System.out.println("Name-->"+(i+1)+": "+  arr.get(i));
+       }
+
+    }
+}
